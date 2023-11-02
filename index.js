de34089f1b770650fb6700f58c16461c737033a7
FelipeGuimaraes1 <fil-2014@hotmail.com>
2023-11-02 09:53:17 -0300

    Alteração na forma de sortear

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 function generateNumber(){
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(result)
+    if(min >= max) {
+        alert("O primeiro valor deve ser MENOR que o segundo")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        alert(result)
+    }
 }
 
 drawButton = document.querySelector(".draw-button")
